600a871ec4ad65fdf003362ddcbecbd66b988faf
John <johnnonso090@gmail.com>
2021-10-23 20:49:08 +0100

    added the custom students controller

---
+++ app/controllers/api/v1/custom_students_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::CustomStudentsController < ApplicationController
+    include PermissionHelper
+    before_action :authenticate_api_v1_admin!, only: [:create]
+    before_action :find_admin, only: [:create]
+    before_action :figure_status, only: [:create]
+   
+    def create 
+
+        
+        successful = false
+        result = Cloudinary::Uploader.upload(params[:image], options = {})
+        
+        debugger
+        @student = Student.new student_params
+        Student.transaction(requires_new: true) do 
+
+            @student.school = @admin.school
+            @student.email = "#{student_params[:first_name]}#{student_params[:last_name]}888@school.edu"
+            @student.password = "#{student_params[:first_name]}888"
+            
+            @student.image = result['url']
+            @student.save
+
+            if @student.save 
+                successful = true 
+
+            end
+            
+
+        end
+
+
+        if successful 
+            render json: @student, status: :created 
+
+        else 
+            render json: @student.errors.messages, status: :unprocessable_entity
+        end
+
+        
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :middle_name, :classroom_id)
+    end
+
+    def find_admin 
+        @admin = current_api_v1_admin
+    end
+
+    def figure_status
+        check_permission_for @admin
+    end
+end
