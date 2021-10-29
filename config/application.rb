a5634ac9e730496689dc6040fd2379bbfe92c089
John <johnnonso090@gmail.com>
2021-10-29 20:46:55 +0100

    fixed keys

--- config/application.rb
+++ config/application.rb
@@ -40,12 +40,14 @@ module SchoolB
 
      # This line goes to the head of the file
     # One-time, on the server
-    
+
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'webpush.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
+    
+    
   end
 end
--- spec/requests/api/v1/vapid_keys_spec.rb
+++ spec/requests/api/v1/vapid_keys_spec.rb
@@ -1,21 +1,20 @@
+require 'rails_helper'
 
- require 'rails_helper'
-
-# RSpec.describe "Api::V1::VapidKeys", type: :request do
-#   describe "Get" do
+RSpec.describe "Api::V1::VapidKeys", type: :request do
+  describe "POST" do
    
     
 
    
-#     # context "when new webpush notifications has been created " do 
+    context "when new webpush notifications has been created " do 
 
-#     #    it "increment web_push_notification by 1" do
-#     #       get 'api/v1/vapid_keys' 
-#     #       json_body = JSON.parse(response.body)
-#     #     end
+       it "increment web_push_notification by 1" do
+          get '/api/v1/vapid_keys' 
+          
+        end
 
-#     # end
+    end
 
      
-#   end
-# end
+  end
+end
