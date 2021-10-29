6c197b7a66bf003ea28251d28e494b60edd09f6f
John <johnnonso090@gmail.com>
2021-10-29 20:32:21 +0100

    changed the keys

--- app/controllers/api/v1/vapid_keys_controller.rb
+++ app/controllers/api/v1/vapid_keys_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::VapidKeysController < ApplicationController
 
 	def index 
 
-		@vapid_key = Base64.urlsafe_decode64(Rails.application.credentials.dig(:webpush, :public_key))
+		@vapid_key = Base64.urlsafe_decode64(ENV['VAPID_PUBLIC_KEY'])
 
 
 		render json: @vapid_key, status: :ok
