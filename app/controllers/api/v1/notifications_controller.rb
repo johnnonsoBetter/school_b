cb7aa927ad1eda51f86475eb117406983b627469
John <johnnonso090@gmail.com>
2021-10-29 09:27:49 +0100

    env

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::NotificationsController < ApplicationController
   def create
 
-    notification = WebPushNotification.new (
+    # notification = WebPushNotification.new (
 
-        endpoint: params[:endpoint],
-        auth_key: params[:keys][:auth],
-        p256dh_key: params[:keys][:p256dh],
+    #     endpoint: params[:endpoint],
+    #     auth_key: params[:keys][:auth],
+    #     p256dh_key: params[:keys][:p256dh],
 
-      )
+    #   )
 
 
-    if notification.save 
-      render json: notification, status: :created 
-    else
-      render json: notification.errors.messages, status: :unprocessable_entity
-    end
+    # if notification.save 
+    #   render json: notification, status: :created 
+    # else
+    #   render json: notification.errors.messages, status: :unprocessable_entity
+    # end
   end
 end
