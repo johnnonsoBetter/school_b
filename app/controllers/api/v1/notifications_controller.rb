1968bb232efc1a5f8b6484abe6ff2855df1762cb
John <johnnonso090@gmail.com>
2021-10-29 11:34:49 +0100

    fixed the notifications conroller

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,19 +1,14 @@
 class Api::V1::NotificationsController < ApplicationController
   def create
 
-    # notification = WebPushNotification.new (
-
-    #     endpoint: params[:endpoint],
-    #     auth_key: params[:keys][:auth],
-    #     p256dh_key: params[:keys][:p256dh],
-
-    #   )
+    notification = WebPushNotification.new endpoint: params[:endpoint], auth_key: params[:keys][:auth], p256dh_key: params[:keys][:p256dh],
 
 
-    # if notification.save 
-    #   render json: notification, status: :created 
-    # else
-    #   render json: notification.errors.messages, status: :unprocessable_entity
-    # end
+
+    if notification.save 
+      render json: notification, status: :created 
+    else
+      render json: notification.errors.messages, status: :unprocessable_entity
+    end
   end
 end
