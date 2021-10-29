4fa55d106e0b4829d0ea332a678b256e55250b45
John <johnnonso090@gmail.com>
2021-10-29 13:24:55 +0100

    created test for the create action of the notifications controller

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -6,17 +6,22 @@ class Api::V1::NotificationsController < ApplicationController
 
 
 
-    #  notification = WebPushNotification.new endpoint: params[:endpoint], auth_key: params[:keys][:auth], p256dh_key: params[:keys][:p256dh]
+     notification = WebPushNotification.new 
+     notification.endpoint = params[:endpoint]
+    # t.string "endpoint"
+    # t.string "auth_key"
+    # t.string "p256dh_key"
+    # t.datetime "created_at", precision: 6, null: false
+    # t.datetime "updated_at", precision: 6, null: false
+
+      puts  "this is the notification", notification
 
 
-      puts  params
-
-
-    # if notification.save 
-    #   render json: notification, status: :created 
-    # else
-    #   render json: notification.errors.messages, status: :unprocessable_entity
-    # end
+    if notification.save 
+      render json: notification, status: :created 
+    else
+      render json: notification.errors.messages, status: :unprocessable_entity
+    end
 
 
   end
