9732cdd02da1a7c1cef86b3ef2e907589a23e880
John <johnnonso090@gmail.com>
2021-10-29 16:05:22 +0100

    modified the webpushnotification model

--- app/models/web_push_notification.rb
+++ app/models/web_push_notification.rb
@@ -1,2 +1,20 @@
 class WebPushNotification < ApplicationRecord
+	
+
+  	def send(message) 
+
+  		Webpush.payload_send(
+		    message: message,
+		    endpoint: endpoint,
+		    p256dh: p256dh_key,
+		    auth: auth_key,
+		    vapid: {
+		      public_key: ENV['VAPID_PUBLIC_KEY'],
+		      private_key: ENV['VAPID_PRIVATE_KEY']
+		    },
+	    
+	  	)
+
+
+  	end
 end
