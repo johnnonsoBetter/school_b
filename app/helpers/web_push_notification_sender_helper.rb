94fdeb1dff37e92cd4e2981e3f6481158699f81f
John <johnnonso090@gmail.com>
2021-11-11 16:41:06 +0100

    fixed the notification

--- app/helpers/web_push_notification_sender_helper.rb
+++ app/helpers/web_push_notification_sender_helper.rb
@@ -12,12 +12,8 @@ module WebPushNotificationSenderHelper
 
         guidance.web_push_notifications.each do |web_push|
             
-
-            begin
-                send_push_notification(message, web_push)
-            rescue  => ex      
-               web_push.destroy
-            end
+          send_push_notification(message, web_push)
+            
 
         end
 
