2ae57d09ae5d6bda33b0cdb1e43151933be320dc
John <johnnonso090@gmail.com>
2021-11-11 15:43:07 +0100

    handled errors on the webpush notifiations

--- app/helpers/web_push_notification_sender_helper.rb
+++ app/helpers/web_push_notification_sender_helper.rb
@@ -6,6 +6,10 @@ module WebPushNotificationSenderHelper
 
       guidances.each do |guidance| 
 
+
+        
+
+
         guidance.web_push_notifications.each do |web_push|
             send_push_notification(message, web_push)
 
@@ -30,17 +34,24 @@ module WebPushNotificationSenderHelper
 
     def send_push_notification(message, webpush)
 
-      Webpush.payload_send(
-        message: JSON.generate(message),
-        endpoint: webpush.endpoint,
-        p256dh: webpush.p256dh_key,
-        auth: webpush.auth_key,
-        vapid: {
-          public_key: ENV['VAPID_PUBLIC_KEY'],
-          private_key: ENV['VAPID_PRIVATE_KEY']
-        },
-    
-      )
+
+
+      begin
+          Webpush.payload_send(
+            message: JSON.generate(message),
+            endpoint: webpush.endpoint,
+            p256dh: webpush.p256dh_key,
+            auth: webpush.auth_key,
+            vapid: {
+              public_key: ENV['VAPID_PUBLIC_KEY'],
+              private_key: ENV['VAPID_PRIVATE_KEY']
+            },
+        
+          )
+      rescue StandardError => ex      
+          puts  "this webpush has expired"
+      end
+
 
 
   end
